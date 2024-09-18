064e6f90db667a6c9301c4b65d7dc20cc89ed018
kennert nurja <nurjakennert@gmail.com>
2024-09-18 19:56:29 +0300

    create new app

---
+++ myfirstapp/Program.cs
@@ -0,0 +1,13 @@
+﻿// this is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//2 tervitabkasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !"); //Output
